5b3c819ba6c1c3438b3cb86577f336eb4f1dbd72
Nate Wolfe <nwolfe@puppetlabs.com>
2014-01-22 14:26:09 -0800

    (PE-2604) Delete unnecessary unit test

--- project.clj
+++ project.clj
@@ -2,11 +2,10 @@
   :url "http://www.github.com/puppetlabs/jvm-certificate-authority"
   :dependencies [[org.clojure/clojure "1.5.1"]]
   :source-paths ["src/main/clojure"]
-  :test-paths ["test/clojure"]
   :java-source-paths ["src/main/java"]
   :profiles {:dev {:dependencies [[puppetlabs/trapperkeeper "0.1.0"] [me.raynes/fs "1.4.3"]]
                    :main puppetlabs.jvm.certificate-authority.server
-                   :source-paths ["src/main/clojure" "src/test/clojure"]
+                   :source-paths ["src/test/clojure"]
                    :aliases {"server" ["trampoline" "run"
                                        "--config" "test-resources/config.ini"
                                        "--bootstrap-config" "test-resources/bootstrap.cfg"]}}})
