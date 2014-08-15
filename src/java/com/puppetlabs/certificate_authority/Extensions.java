5a151c61f3d776080bd49593d4ed7b0dda491df7
Kevin Corcoran <kevin.corcoran@puppetlabs.com>
2014-08-15 14:51:06 -0700

    add constants for extension OIDs

---
+++ src/java/com/puppetlabs/certificate_authority/Extensions.java
@@ -0,0 +1,6 @@
+package com.puppetlabs.certificate_authority;
+
+public class Extensions {
+    public static final String ppRegCertExt = "1.3.6.1.4.1.34380.1.1";
+    public static final String ppPrivCertExt = "1.3.6.1.4.1.34380.1.2";
+}
