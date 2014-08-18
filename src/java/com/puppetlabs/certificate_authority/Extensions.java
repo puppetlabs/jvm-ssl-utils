fe65d5d40c4e4a394eb537bb406f06c00c4f88fb
Kevin Corcoran <kevin.corcoran@puppetlabs.com>
2014-08-18 12:39:13 -0700

    (PE-3864) delete 'Extensions' class

--- src/java/com/puppetlabs/certificate_authority/Extensions.java
+++
@@ -1,6 +0,0 @@
-package com.puppetlabs.certificate_authority;
-
-public class Extensions {
-    public static final String ppRegCertExt = "1.3.6.1.4.1.34380.1.1";
-    public static final String ppPrivCertExt = "1.3.6.1.4.1.34380.1.2";
-}
