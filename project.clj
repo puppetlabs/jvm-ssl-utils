79927b20b431e38079991b03ee47e090c8dff3ae
Joe Pinsonault <joe.pinsonanult@puppetlabs.com>
2015-01-22 11:53:54 -0800

    Updated version in project.clj

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
     :password :env/clojars_jenkins_password
     :sign-releases false })
 
-(defproject puppetlabs/ssl-utils "0.6.2-SNAPSHOT"
+(defproject puppetlabs/ssl-utils "0.7.0-SNAPSHOT"
   :url "http://www.github.com/puppetlabs/jvm-ssl-utils"
 
   :description "SSL certificate management on the JVM."
