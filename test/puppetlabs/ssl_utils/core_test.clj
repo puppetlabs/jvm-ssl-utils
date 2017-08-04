145d16eb477cc550b28606d72247909acc4aae82
Adrien Thebo <git@somethingsinistral.net>
2017-08-04 13:47:17 -0700

    (maint) Update CA files for fixture data
    This commit copies in the CA files as test fixtures; the inclusion of
    the private keys will allow us to update the existing fixtures and
    generate more certificates without having to rebuild the entire CA state
    on each update.

--- test/puppetlabs/ssl_utils/core_test.clj
+++ test/puppetlabs/ssl_utils/core_test.clj
@@ -876,9 +876,9 @@
   (testing "certificate"
     (let [cert (pem->cert (open-ssl-file "certs/localhost.pem"))]
       (are [algorithm expected] (= expected (fingerprint cert algorithm))
-           "SHA-1"   "2232c6267b24f9eaf9323cdf4ed07e31a131202d"
-           "SHA-256" "8c8438c39f6505d9766d335d66b081034a83f2a56c41704758f87fc26c76ef46"
-           "SHA-512" "2ad13f9b0aa4d0cca69ea4c1e1f1543461774c74ca99340c8b1264e5c8012402019ba0ab807e648847c29819e3c058f1b9df1a10ce4310c8a8c0d49c9b67e269")))
+           "SHA-1"   "8541015c004a0e780fea9d34e85508223540f8db"
+           "SHA-256" "d23bad16a87151e24bfd05b0aa7c82423d0d74889e7316eddc4384b7f6c95196"
+           "SHA-512" "71f35390978f0345fbb7f6cbc4be992dcdc07648879d0db836a0a49f637932a4abb4619e37fe9320af5634097204b6d7d2d3326d29d901c95c76e8f2b64e6ce4")))
 
   (testing "csr"
     (let [csr (pem->csr (open-ssl-file "certification_requests/ca_test_client.pem"))]
