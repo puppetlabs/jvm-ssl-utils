ebbf2b793733790e6e41cdc50b99a4f0ad30ef70
Jenkins CI <ci@puppet.com>
2018-09-21 21:55:49 +0000

    Version 1.0.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
     :password :env/clojars_jenkins_password
     :sign-releases false })
 
-(defproject puppetlabs/ssl-utils "1.0.1"
+(defproject puppetlabs/ssl-utils "1.0.2-SNAPSHOT"
   :url "http://www.github.com/puppetlabs/jvm-ssl-utils"
 
   :description "SSL certificate management on the JVM."
